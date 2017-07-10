6cd6b377dee3d824c23a0814aa7808de7215408d
beeftosino <lestgabo@gmail.com>
2017-07-10 06:23:59 +0000

    products look organized

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -2,5 +2,19 @@ module Spree::BaseHelper
     def logo(image_path = Spree::Config[:logo], img_options: {})
       link_to image_tag(image_path, img_options), spree.root_path
     end
+    
+    def nav_tree(root_taxon, current_taxon, max_level = 1)
+      return '' if max_level < 1 || root_taxon.children.empty?
+      content_tag :ul, class: 'dropdown-menu' do
+        taxons = root_taxon.children.map do |taxon|
+          css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'active' : nil
+          content_tag :li, class: css_class do
+           link_to(taxon.name, seo_url(taxon)) +
+             taxons_tree(taxon, current_taxon, max_level - 1)
+          end
+        end
+        safe_join(taxons, "\n")
+      end
+    end
 end
     
